d6fa64a54de90163588af8f11c49a34f885fa204
lucasoal <lucasoalzst@gmail.com>
2021-08-01 03:54:52 -0300

    personalização e adaptação do css

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,14 +1,12 @@
 import "./style.css";
 
 function CardGame() {
-
-  function alert() {
-    alert("CLICK!");
-  }
-
   return `
-    <button class="card-game" onclick="alert()">
-      <img src="images/alura-pixel.png" alt="Logo da Alura">
+    <button class="card-game" onclick="alert('Click')">
+      <img src="images/interrogationPoint.png"
+        alt="Logo da Alura"
+        weight="60px" height="95px"
+      >
     </button>
   `;
 }
